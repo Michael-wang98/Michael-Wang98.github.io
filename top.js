a42c4baf425978f60edeec176070bc93c8c0b01f
Michael Wang <michael.wang1134@gmail.com>
2020-12-16 17:16:20 -0500

    Update top.js

--- top.js
+++ top.js
@@ -28,3 +28,13 @@ scrollToTopButton.onclick = function(e) {
   e.preventDefault();
   scrollToTop();
 }
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "350px";
+    document.getElementById("main").style.marginRight = "350px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+}
